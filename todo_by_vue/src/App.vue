6afab6f94053f5bc158603f6c4d3b53fba6026ff
jbkim4040 <83804185+jbkim4040@users.noreply.github.com>
2024-01-08 17:56:09 +0900

    컴포넌트 및 함수 추가

--- todo_by_vue/src/App.vue
+++ todo_by_vue/src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TodoForm></TodoForm>
+  <TodoViewer></TodoViewer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoForm from "./components/TodoForm.vue";
+import TodoViewer from "./components/TodoViewer.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    // HelloWorld,
+    TodoForm,
+    TodoViewer
+},
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ todo_by_vue/src/components/TodoInfo.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>{{todo}} 할일 정보 입니당!</h1>
+</template>
+<script setup>
+import {ref} from 'vue'
+const todo = ref("1");
+
+</script>
+<style>
+
+</style>
